4688d43d2c8dbcca109c30eb67d60d278ca1d87d
gaiekdacosta <pelipe.gaiek1701@gmail.com>
2024-10-15 23:01:58 -0300

    Adasd

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,69 @@
 import './index.css'
+import logo from '../public/logo.png'
 import WhatsappButton from './components/whatsappButton'
 
 function App() {
   return (
     <>
-      <section className='flex justify-around mr-8 ml-8 mt-48'>
-        <div className='flex flex-col w-[50%] gap-2'>
-          <div className='text-[25px]'>
-            <img alt="logo" />
-            <p className='font-extrabold leading-7'>
-              Transforme sua autoestima com a 
-              expertise de uma equipe 
-              <span className='text-primary ml-1'>
-                especializada em estética
-              </span>
-            </p>
-            <div className='flex flex-col gap-5 mt-2'>
-              <p>
-                Oferecemos um serviço Premium que vai além do sorriso, 
-                focando na harmonia e funcionalidade dos seus dentes. 
+      <section className='h-[100vh] bg-[url("public/tai9.jpg")] bg-cover bg-center'>
+        <div className='flex justify-around h-[100%] items-center'>
+          <div className='flex flex-col w-[50%] gap-2 p-4 rounded-lg'>
+            <div className='text-[25px] mt-[-5%]'>
+              <img 
+                className='h-16 w-16'
+                src={logo}
+                alt="logo" 
+              />
+              <p className='font-black leading-7'>
+                Transforme sua autoestima com a 
+                expertise de uma equipe 
+                <span className='text-primary ml-1'>
+                  especializada em estética
+                </span>
               </p>
-              <p>
-                Sinta-se confiante em todas as ocasiões e experimente a 
-                excelência em cada detalhe do seu tratamento.
-              </p>
+              <div className='flex flex-col gap-5 mt-2  leading-7'>
+                <p>
+                  Oferecemos um serviço Premium que vai além do sorriso, 
+                  focando na harmonia e funcionalidade dos seus dentes. 
+                </p>
+                <p>
+                  Sinta-se confiante em todas as ocasiões e experimente a 
+                  excelência em cada detalhe do seu tratamento.
+                </p>
+              </div>
+            </div>
+            <div className='w-[60%]'>
+              <WhatsappButton content={'AGENDAR UMA CONSULTA AGORA'} />
             </div>
           </div>
-          <div className='w-[60%]'>
-            <WhatsappButton content={'AGENDAR UMA CONSULTA AGORA'} />
+          <div>
+            <iframe 
+              width="560" 
+              height="315" 
+              src="https://www.youtube.com/embed/v0dMmEt98Ro" 
+              allowFullScreen
+            />
           </div>
         </div>
-        <div>
-          <iframe 
-            width="560" 
-            height="315" 
-            src="https://youtu.be/v0dMmEt98Ro?si=-GHxVhuKCZ6-a3ZKm" 
-            allowFullScreen
-          />
-        </div>
+      </section>
+      {/* SERVIÇOS */}
+      <section className='text-center '>
+        <h3 
+          className='
+            font-light
+            text-primary 
+            text-[22px] 
+            tracking-[0.30rem]'
+        >
+          SERVIÇOS
+        </h3>
+        <h4 className='text-[25px] font-bold mt-[-0.5%]'>
+          Quais os nossos procedimentos {' '}
+          <span className='text-primary'>
+            mais procurados?
+          </span>
+        </h4>
+        {/* INSERIR CARDS DE PROCEDIMENTOS AQUI */}
       </section>
     </>
   )
